6e9ad466338866d2565cdc98c4f15ea15fcf899b
Max van Asten <maxvasten@gmail.com>
2022-01-05 15:10:17 +0100

    Added resizing & css stuff

--- client/js/main.js
+++ client/js/main.js
@@ -8,11 +8,19 @@ const init = () => {
     CTX = CANVAS.getContext('2d');
 }
 
+//Runs when the screen is resized
+const resizeCanvas = () => {
+    CANVAS.width = innerWidth;
+    CANVAS.height = innerHeight;
+}
+
 //Runs at 60 fps
 const draw = () => {
     CTX.fillStyle = "#545454";
     CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
 }
+
+window.addEventListener("resize", resizeCanvas);
 
 
 
