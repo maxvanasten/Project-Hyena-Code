bc0a1905996edbfe27d1b6803cf0476c9e69439b
Max van Asten <maxvasten@gmail.com>
2022-01-05 15:35:42 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('../client'))
 
 io.on('connection', (socket) => {
   console.log('a user connected');
