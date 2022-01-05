ce54f7a1137fa2b3020e17846d575673399a3711
Max van Asten <maxvasten@gmail.com>
2022-01-05 15:32:15 +0100

    Added socket.io basics

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 80
 
+const { Server } = require("socket.io");
+const io = new Server(app);
+
 app.use(express.static('../client'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
